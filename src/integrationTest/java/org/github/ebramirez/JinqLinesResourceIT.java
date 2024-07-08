1e8a45c57f0f16b5ceaf17967551144095144b55
Eduardo Ramirez <eramirez@redhat.com>
2024-07-08 17:56:27 +0200

    Use H2 for ITs

--- src/integrationTest/java/org/github/ebramirez/JinqLinesResourceIT.java
+++ src/integrationTest/java/org/github/ebramirez/JinqLinesResourceIT.java
@@ -1,8 +1,11 @@
 package org.github.ebramirez;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class JinqLinesResourceIT extends JinqLinesResourceTest {
 
     // Execute the same tests but in native mode.
